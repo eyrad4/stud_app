87639597d905d898525d045b1ed05aa9d910ccee
ddavidov <ddavidov@mindk.com>
2017-12-21 11:14:15 +0200

    chore(app): Comments added

---
+++ README.md
@@ -0,0 +1,5 @@
+### MindK S.P.A. Course sample API application 
+
+Created as example for S.P.A. Course listeners
+
+*(c) 2017 MindK*
--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 
+// Use Composer autoloader:
 $loader = require_once "../vendor/autoload.php";
 
+// Init and run the application:
 $app = new \Mindk\Framework\App();
 $app->run();
 
