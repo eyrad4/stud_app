6a3e0399085cac9b7f301038eb26555aec57a217
ddavidov <ddavidov@mindk.com>
2018-01-25 10:46:04 +0200

    feat(controllers): Methods implementation added

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -14,6 +14,6 @@ class Controller
      * Home page
      */
     public function home(){
-
+        echo 'Hello!';
     }
 }
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -14,20 +14,20 @@ class ProductController
      * Products index page
      */
     function index(){
-
+        echo 'Product list to be shown here';
     }
 
     /**
      * Single product page
      */
-    function show(){
-
+    function show($id){
+        echo 'Product ID > ' . $id . ' card';
     }
 
     /**
      * Create product
      */
     function create(){
-
+        echo 'creating the product...';
     }
 }
